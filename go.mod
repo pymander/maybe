67c160b19e536dfd4a32f1686788e3378dc23c3f
Erik L. Arneson <earneson@arnesonium.com>
2025-02-12 14:52:59 -0800

    Create the maybe program

---
+++ go.mod
@@ -0,0 +1,3 @@
+module github.com/pymander/maybe
+
+go 1.23.3
---
+++ main.go
@@ -0,0 +1,39 @@
+package main
+
+import (
+	"flag"
+	"fmt"
+	"math/rand"
+	"os"
+)
+
+func main() {
+	var chance int
+	var verbose bool
+
+	flag.IntVar(&chance, "chance", 50, "Percent chance from 1 to 99")
+	flag.BoolVar(&verbose, "verbose", false, "Be noisy")
+	flag.Parse()
+
+	if chance < 1 {
+		chance = 1
+	}
+
+	if chance > 99 {
+		chance = 99
+	}
+
+	num := rand.Intn(100) // random int 0-99
+
+	if num < chance {
+		if verbose {
+			fmt.Printf("The %d percent chance succeeded!\n", chance)
+		}
+		os.Exit(0)
+	} else {
+		if verbose {
+			fmt.Printf("The %d percent chance failed!\n", chance)
+		}
+		os.Exit(1)
+	}
+}
